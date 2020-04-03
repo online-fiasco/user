41e04434e6cd231885127cd18401df5fc660646e
XxshiftxX <contact@dev-shift.me>
2020-04-03 17:04:03 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  rules: {
+    'no-console': false,
+  }
+};
