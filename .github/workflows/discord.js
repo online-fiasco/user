88682f073ab649795bc809292f2b7601bf58882e
XxshiftxX <contact@dev-shift.me>
2020-04-06 15:58:10 +0900

    edit main.yml

--- .github/workflows/discord.js
+++ .github/workflows/discord.js
@@ -1,5 +1,42 @@
 const axios = require('axios');
 
-const isSucceed = process.env.SUCCESS
+const webhook = process.env.WEBHOOK;
+const messageType = process.env.MESSAGE_TYPE;
 
-console.log(isSucceed)
+const getInfoMessage = () => (
+  {
+    "embeds": [
+      {
+        "fields": [
+          {
+            "name": "Repository",
+            "value": "[online-fiasco/user](https://github.com/online-fiasco/user)",
+            "inline": true
+          },
+          {
+            "name": "Branch",
+            "value": "[feature/user-db](https://github.com/online-fiasco/user/tree/feature/user-db)",
+            "inline": true
+          }
+        ],
+        "title": ":sparkles: New commit: a3988e5 :sparkles:",
+        "description": "add dependency for database and test\n\n- mongoose: MongoDB ORM\n- sinon: Test mocking\n- crypto: Encryption",
+        "footer": {
+          "text": "commit by XxshiftxX",
+          "icon_url": "https://avatars0.githubusercontent.com/u/26434484?s=460&u=9c07530310129def4870d8091ceb7263bcfc8dff&v=4"
+        }
+      }
+    ]
+  }
+);
+
+const getPayload = () => {
+  switch(messageType)
+  {
+    case 'info':
+      return getInfoMessage();
+      break;
+  }
+}
+
+axios.post(webhook, getPayload())
