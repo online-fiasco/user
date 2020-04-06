b88dc64c221754697d408057b1023c6cc6479561
XxshiftxX <contact@dev-shift.me>
2020-04-06 17:12:43 +0900

    edit main.yml (multiline test)
    - multiline test.
    - it should be sent to discord
    - good luck :)

--- .github/workflows/discord.js
+++ .github/workflows/discord.js
@@ -6,29 +6,29 @@ const github = JSON.parse(process.env.GITHUB);
 
 const getInfoMessage = () => (
   {
-    "embeds": [
+    embeds: [
       {
-        "fields": [
+        fields: [
           {
-            "name": "Repository",
-            "value": `[${github.repository}](https://github.com/${github.repository})`,
-            "inline": true
+            name: 'Repository',
+            value: `[${github.repository}](https://github.com/${github.repository})`,
+            inline: true
           },
           {
-            "name": "Branch",
-            "value": `[${github.ref.replace('refs/heads/', '')}](https://github.com/${github.repository}/tree/${github.ref.replace('refs/heads/', '')})`,
-            "inline": true
+            name: 'Branch',
+            value: `[${github.ref.replace('refs/heads/', '')}](https://github.com/${github.repository}/tree/${github.ref.replace('refs/heads/', '')})`,
+            inline: true
           },
           {
-            "name": "Commit",
-            "value": `[${github.event.after.slice(0, 7)}](https://github.com/${github.repository}/commit/${github.event.after})`
+            name: 'Commit',
+            value: `[${github.event.after.slice(0, 7)}](https://github.com/${github.repository}/commit/${github.event.after})`
           }
         ],
-        "title": `:sparkles: New commit: ${github.event.after.slice(0, 7)} :sparkles:`,
-        "description": github.event.head_commit.message,
-        "footer": {
-          "text": `commit by ${github.actor}`,
-          "icon_url": github.event.sender.avatar_url,
+        title: `:sparkles: New commit: ${github.event.after.slice(0, 7)} :sparkles:`,
+        description: github.event.head_commit.message,
+        footer: {
+          text: `commit by ${github.actor}`,
+          icon_url: github.event.sender.avatar_url,
         }
       }
     ]
@@ -36,21 +36,18 @@ const getInfoMessage = () => (
 );
 
 const getPayload = () => {
-  switch(messageType)
-  {
+  switch (messageType) {
     case 'info':
       return getInfoMessage();
     case 'test-success':
       return {
-        "content": ":white_check_mark: <@&696626988147933215> Test succeed! Good job :white_check_mark:"
+        content: ':white_check_mark: <@&696626988147933215> Test succeed! Good job :white_check_mark:'
       };
     case 'test-failed':
       return {
-        "content": `:octagonal_sign: <@&696626988147933215> Test failed! Please check in [here](https://github.com/${github.repository}/actions) :octagonal_sign:`
+        content: `:octagonal_sign: <@&696626988147933215> Test failed! Please check in [here](https://github.com/${github.repository}/actions) :octagonal_sign:`
       }
   }
 }
-
-console.log(webhook, getPayload());
 
 axios.post(webhook, getPayload())
