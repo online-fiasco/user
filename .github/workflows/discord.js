6bfc7689731f27515ca27d81db4b65e443392689
XxshiftxX <contact@dev-shift.me>
2020-04-06 16:42:25 +0900

    edit main.yml

--- .github/workflows/discord.js
+++ .github/workflows/discord.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const webhook = process.env.WEBHOOK;
 const messageType = process.env.MESSAGE_TYPE;
-const github = process.env.GITHUB;
+const github = JSON.parse(process.env.GITHUB);
 
 const getInfoMessage = () => (
   {
@@ -11,20 +11,24 @@ const getInfoMessage = () => (
         "fields": [
           {
             "name": "Repository",
-            "value": "[online-fiasco/user](https://github.com/online-fiasco/user)",
+            "value": `[${github.repository}](https://github.com/${github.repository})`,
             "inline": true
           },
           {
             "name": "Branch",
-            "value": "[feature/user-db](https://github.com/online-fiasco/user/tree/feature/user-db)",
+            "value": `[${github.ref.replace('refs/heads/', '')}](https://github.com/${github.repository}/tree/${github.ref.replace('refs/heads/', '')})`,
             "inline": true
+          },
+          {
+            "name": "Commit",
+            "value": `[${github.event.after.slice(0, 7)}](https://${github.repository}/commit/${github.event.after})`
           }
         ],
-        "title": ":sparkles: New commit: a3988e5 :sparkles:",
-        "description": "add dependency for database and test\n\n- mongoose: MongoDB ORM\n- sinon: Test mocking\n- crypto: Encryption",
+        "title": `:sparkles: New commit: ${github.event.after.slice(0, 7)} :sparkles:`,
+        "description": github.event.commits.map(commit => commit.id === github.event.after)[0].message,
         "footer": {
-          "text": "commit by XxshiftxX",
-          "icon_url": "https://avatars0.githubusercontent.com/u/26434484?s=460&u=9c07530310129def4870d8091ceb7263bcfc8dff&v=4"
+          "text": `commit by ${github.actor}`,
+          "icon_url": github.event.sender.avatar_url,
         }
       }
     ]
